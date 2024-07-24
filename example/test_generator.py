ac85e34fb63dc81aa4e3c93197f3d0a9e7ed9191
Dmitriy <sdd@lab.local>
2024-07-24 15:57:07 +0700

    ⚗️ start gitlab api

--- example/test_generator.py
+++ example/test_generator.py
@@ -6,3 +6,21 @@ from generator_bdd.generator import Person
 person = Person("Di", 25)
 
 person.say_hello()
+
+
+# from src.utils.api
+# _gitlab import create
+# _cumber
+# 2
+# 3
+# 4
+# 5
+# 7
+# 9
+# 10
+# 11
+# create cumber
+# group_id = 137,
+# epi_11d = 2,
+# name file= test generator' scenario_number='scr-1-1,' generator=True
+# # Пример для эксперементов generator
