a6d8f32c25b6aeb0385b8645fa35208d2d007ffd
Dmitriy <sdd@lab.local>
2024-07-25 07:33:02 +0700

    📝 change main page

--- docs/conf.py
+++ docs/conf.py
@@ -17,9 +17,9 @@
 
 # -- Project information -----------------------------------------------------
 
-project = "Basic Sphinx Example Project"
-copyright = "2022, Read the Docs core team"
-author = "Read the Docs core team"
+project = "Cucmber generator"
+copyright = "2024, Shibikin Dmitry"
+author = "Shibikin Dmitry"
 
 
 # -- General configuration ---------------------------------------------------
